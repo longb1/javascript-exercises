c6afb0d928165909acc4428a78b04416536f64aa
Long <17272827+longb1@users.noreply.github.com>
2022-04-08 00:53:45 +0100

    Completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let results=[]
+    for(let i =0;i<books.length;i++){
+        results.push(books[i].title)
+    }
+    return results
 };
 
 // Do not edit below this line
